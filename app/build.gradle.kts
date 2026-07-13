7722d51fcabc47285ce7eabe3368a96cd3594990
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2026-07-13 11:38:23 +0200

    add release file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,16 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
