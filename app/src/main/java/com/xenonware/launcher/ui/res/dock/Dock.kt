9e4f0882f7599beca2b24370a82136a1d6015f24
Xenon-Ware <116182930+Dinico414@users.noreply.github.com>
2026-07-23 01:09:09 +0200

    stop ime safedraw usage for dock

--- app/src/main/java/com/xenonware/launcher/ui/res/dock/Dock.kt
+++ app/src/main/java/com/xenonware/launcher/ui/res/dock/Dock.kt
@@ -29,9 +29,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -213,7 +213,7 @@ fun DockPill(
     }
 
     val baseDockColor = colorScheme.surfaceContainer
-    val safeDrawBottom = WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom).asPaddingValues().calculateBottomPadding()
+    val safeDrawBottom = WindowInsets.navigationBars.only(WindowInsetsSides.Bottom).asPaddingValues().calculateBottomPadding()
     val bottomPadding = if (safeDrawBottom < 16.dp) {16.dp} else {safeDrawBottom + 8.dp}
 
     Row(
