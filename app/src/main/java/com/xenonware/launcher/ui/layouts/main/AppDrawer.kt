c079b3a2fe6a53333fdc708bc252d5daed3dad88
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2026-09-03 21:53:42 +0200

    add fullscreen drawer for fullscreen

--- app/src/main/java/com/xenonware/launcher/ui/layouts/main/AppDrawer.kt
+++ app/src/main/java/com/xenonware/launcher/ui/layouts/main/AppDrawer.kt
@@ -31,6 +31,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -38,6 +40,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
@@ -538,7 +541,13 @@ fun AppDrawer(
                 .graphicsLayer(clip = false)
                 .background(
                     containerColor,
-                    RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+                    run {
+                        val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+                        RoundedCornerShape(
+                            topStart = if (statusBarHeight > 0.dp) 32.dp else 0.dp,
+                            topEnd = if (statusBarHeight > 0.dp) 32.dp else 0.dp
+                        )
+                    }
                 )
                 .clickable(enabled = false) { }
         ) {
